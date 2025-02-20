24dd2a3d3359363fa19bd5291e38687f46ca5d86
RayhanRamirez <125675560+raynayam@users.noreply.github.com>
2025-02-20 11:36:33 -0600

    Add files via upload

---
+++ Azure-Web-App/variables.tf
@@ -0,0 +1,9 @@
+variable "azurerm_resource_group" {
+    description = "Name of the resource group"
+    default = "cloud-engineer-rg"
+}
+
+variable "location" {
+    description = "Azure Region"
+    default = "East US"
+}
